9a1f51faf28d1b767e1f6ad7a3431419452b0c2b
Tetchenko Dima <tetchenko@gmail.com>
2024-05-15 23:55:35 +0300

    Задача спринта №8

---
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,16 @@
+package com.practicum.playlistmaker
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        findViewById<ImageView>(R.id.back_button).setOnClickListener {
+            finish()
+        }
+    }
+}
