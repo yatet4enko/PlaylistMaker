883814367de40e02435f562c243524e1d932b80a
Tetchenko Dima <tetchenko@gmail.com>
2024-05-21 17:08:03 +0300

    правки по ревью 2

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,16 +1,21 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        findViewById<ImageView>(R.id.back_button).setOnClickListener {
-            finish()
+        findViewById<Toolbar>(R.id.toolbar).let {
+            title = ""
+            setSupportActionBar(it)
+            title = resources.getString(R.string.settings)
+
+            supportActionBar?.setDisplayShowHomeEnabled(true)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
     }
 }
