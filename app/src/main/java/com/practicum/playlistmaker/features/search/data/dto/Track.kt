4bb5451f2650d32d896ce0d02a772ded7a74a45d
Tetchenko Dima <tetchenko@gmail.com>
2024-06-15 17:25:17 +0300

    Задача спринта №10

---
+++ app/src/main/java/com/practicum/playlistmaker/features/search/data/dto/Track.kt
@@ -0,0 +1,8 @@
+package com.practicum.playlistmaker.features.search.data.dto
+
+data class Track(
+    val trackName: String,
+    val artistName: String,
+    val trackTime: String,
+    val artworkUrl100: String,
+)
