fd1e6fefd16c533bcdd2ecea5ac9217c9e3a3935
Tetchenko Dima <tetchenko@gmail.com>
2025-01-10 14:17:40 +0300

    задание спринта номер 21

--- app/src/main/java/com/practicum/playlistmaker/features/search/data/SearchTracksRepositoryImpl.kt
+++ app/src/main/java/com/practicum/playlistmaker/features/search/data/SearchTracksRepositoryImpl.kt
@@ -3,15 +3,19 @@ package com.practicum.playlistmaker.features.search.data
 import com.practicum.playlistmaker.features.media.data.db.AppDatabase
 import com.practicum.playlistmaker.features.search.domain.api.SearchTracksRepository
 import com.practicum.playlistmaker.features.search.domain.models.Track
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
 
 class SearchTracksRepositoryImpl(
     private val searchTracksRemoteDataSource: SearchTracksRemoteDataSource,
     private val db: AppDatabase,
 ): SearchTracksRepository {
     override fun search(text: String): Flow<List<Track>?> = flow {
-        val favoriteTracksIds = db.trackDao().getAllTrackIds()
+        val favoriteTracksIds = withContext(Dispatchers.IO) {
+            db.trackDao().getAllTrackIds()
+        }
 
         emit(
             searchTracksRemoteDataSource
